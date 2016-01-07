46f643732151a46828b2942c1eaec254118eaca1
Akrso <quinn_akrso@kaist.ac.kr>
2016-01-07 15:34:26 +0900

    Battle scene edited

--- PokeDama/Assets/Scripts/UI/BattleUIManager.cs
+++ PokeDama/Assets/Scripts/UI/BattleUIManager.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class BattleUIManager : MonoBehaviour {
 
+	public GameObject inkachu;
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
@@ -14,14 +15,34 @@ public class BattleUIManager : MonoBehaviour {
 			OnEscapeKeyPress();
 		}
 	}
+	void OnEscapeKeyPress() {
+		Debug.Log ("Moving to Map Scene...");
+		Application.LoadLevel ("MapScene");
+	}
+
+
+
+
+
 
 	public void OnRunButtonClick() {
 		Debug.Log ("Moving to Map Scene...");
 		Application.LoadLevel ("MapScene");
 	}
 
-	void OnEscapeKeyPress() {
-		Debug.Log ("Moving to Map Scene...");
-		Application.LoadLevel ("MapScene");
+	public void Kick(){
+		Instantiate (inkachu, new Vector3 (0, 0, 0), Quaternion.identity);
+
 	}
+	public void Throw(){
+
+	}
+	public void ACT110V(){
+
+	}
+	public void Sleep(){
+
+	}
+
+
 }
