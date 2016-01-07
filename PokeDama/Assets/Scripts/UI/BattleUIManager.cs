c8dcf87d8d359540430923b7e3ac482d16a854e1
Akrso <quinn_akrso@kaist.ac.kr>
2016-01-07 23:53:09 +0900

    Branch merge

--- PokeDama/Assets/Scripts/UI/BattleUIManager.cs
+++ PokeDama/Assets/Scripts/UI/BattleUIManager.cs
@@ -2,6 +2,11 @@
 using System.Collections;
 
 public class BattleUIManager : MonoBehaviour {
+	int PlayerStat = 100;
+	int EnemyStat = 100;
+	Random r = new Random();
+	int playset = r.Next(0,4);
+
 
 	public GameObject inkachu;
 	// Use this for initialization
@@ -30,19 +35,55 @@ public class BattleUIManager : MonoBehaviour {
 		Application.LoadLevel ("MapScene");
 	}
 
-	public void Kick(){
-		Instantiate (inkachu, new Vector3 (0, 0, 0), Quaternion.identity);
+	public void PlayerAction(int num){
+		switch (num) {
+		case 0:
+			Kick ();
+			break;
+		case 1:
+			Throw ();
+			break;
+		case 2:
+			ACT110V ();
+			break;
+		case 3:
+			Sleep ();
+			break;
+		}
+		EnemyAction (playset);
+	}
 
+
+	public void Kick(int Stat){
+		Instantiate (inkachu, new Vector3 (0, 0, 0), Quaternion.identity);
+		EnemyStat -= 5;
 	}
 	public void Throw(){
-
+		EnemyStat -= 10;
 	}
 	public void ACT110V(){
-
+		EnemyStat -= 20;
 	}
 	public void Sleep(){
-
+		EnemyStat -= 0;
 	}
+	
 
+	public void EnemyAction(int num){
+		switch (num) {
+		case 0:
+			Kick ();
+			break;
+		case 1:
+			Throw ();
+			break;
+		case 2:
+			ACT110V ();
+			break;
+		case 3:
+			Sleep ();
+			break;
+		}
+	}
 
 }
