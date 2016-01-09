d0d752b73d2648bd48490c7b52c5c071683151b2
andrewpk95 <andrewpk95@hotmail.com>
2016-01-09 15:51:29 +0900

    Added Music, Some changes to ProfileScene

--- PokeDama/Assets/Scripts/GameLogic/AudioManager.cs
+++ PokeDama/Assets/Scripts/GameLogic/AudioManager.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class AudioManager : MonoBehaviour {
 
+	public bool dontDestroyOnLoad;
 	// Use this for initialization
 	void Start () {
-	
+		if (dontDestroyOnLoad) {
+			DontDestroyOnLoad (transform.gameObject);
+		}
 	}
 	
 	// Update is called once per frame
--- PokeDama/Assets/Scripts/GameLogic/ProfileGameManager.cs
+++ PokeDama/Assets/Scripts/GameLogic/ProfileGameManager.cs
@@ -5,9 +5,9 @@ public class ProfileGameManager : MonoBehaviour, GameManager {
 
 	NetworkManager network;
 	PokeDamaManager pokeDamaManager;
+	ShakeDetector shakeDetector;
 
 	PokeDama myPokeDama;
-	int friendliness;
 
 	// Use this for initialization
 	void Start () {
@@ -15,20 +15,31 @@ public class ProfileGameManager : MonoBehaviour, GameManager {
 
 		network = FindObjectOfType<NetworkManager> ();
 		pokeDamaManager = FindObjectOfType<PokeDamaManager> ();
+		shakeDetector = FindObjectOfType<ShakeDetector> ();
 
 		//Debug purposes
-		pokeDamaManager.SaveMyPokeDama (new PokeDama (imei, 1, "inkachu"));
-		myPokeDama = pokeDamaManager.GetMyPokeDama ();
+		//pokeDamaManager.SaveMyPokeDama (new PokeDama (imei, 1, "inkachu"));
+		network.RequestData(imei);
+		//myPokeDama = pokeDamaManager.GetMyPokeDama ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (shakeDetector.isShaked ()) {
+			Debug.Log ("Device Shaken!");
+			Exercise ();
+		}
 	}
 
 	public void Pet() {
-		friendliness++;
-		myPokeDama.friendliness = myPokeDama.friendliness + 1;
+		myPokeDama.friendliness += 2;
+		myPokeDama.recalculateStat ();
+		Save (myPokeDama);
+	}
+
+	public void Exercise() {
+		myPokeDama.friendliness += 10;
+		myPokeDama.recalculateStat ();
 		Save (myPokeDama);
 	}
 
@@ -50,6 +61,27 @@ public class ProfileGameManager : MonoBehaviour, GameManager {
 				Debug.Log (pokeDamaJSON);
 			} else {
 				Debug.Log ("Failed to save your PokeDama...");
+			}
+		}
+
+		//Handling Server Response here
+		if (jsonData.GetField ("ResponseType").str.Equals ("FindByIMEI")) {
+			bool successful = jsonData.GetField ("successful").b;
+			Debug.Log (successful);
+			if (successful) {
+				Debug.Log ("Successfully found your PokeDama!");
+				string pokeDamaJSON = jsonData.GetField ("message").ToString();
+				Debug.Log (pokeDamaJSON);
+				PokeDama inkachu = JsonUtility.FromJson<PokeDama> (pokeDamaJSON);
+
+				pokeDamaManager.SaveMyPokeDama (inkachu);
+				myPokeDama = pokeDamaManager.GetMyPokeDama ();
+				pokeDamaManager.DisplayMyPokeDama (new Vector3(0, 2, 0));
+				//SceneManager.LoadScene ("PokeDamaScene");
+			} else {
+				Debug.Log ("Failed to find your PokeDama...");
+				Debug.Log ("Creating new PokeDama...");
+				//SceneManager.LoadScene ("CreateScene");
 			}
 		}
 	}
