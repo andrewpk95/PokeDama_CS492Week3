25d725c1fccbaa7b0a293a72121bb8a8ce66dbfe
andrewpk95 <andrewpk95@hotmail.com>
2016-01-10 19:18:11 +0900

    Making Create Scene Branch

---
+++ PokeDama/Assets/Scripts/Animation/ProfileAnimationPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProfileAnimationPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
