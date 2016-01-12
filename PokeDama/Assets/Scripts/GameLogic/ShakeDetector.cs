46dee9e809a9ba281a89e5e7abfdb1822e31befe
Jun hong <unique3801@gmail.com>
2016-01-12 21:25:07 +0900

    shake and something fix

--- PokeDama/Assets/Scripts/GameLogic/ShakeDetector.cs
+++ PokeDama/Assets/Scripts/GameLogic/ShakeDetector.cs
@@ -3,19 +3,17 @@ using System.Collections;
 
 public class ShakeDetector : MonoBehaviour
 {
-	public float avrgTime = 0.5f;
-	public float peakLevel = 0.6f;
-	public float endCountTime = 0.6f;
-	public int shakeDir;
-	public int shakeCount;
+    static float accelerometerUpdateInterval = (float)(1.0 / 60.0f);
+    // The greater the value of LowPassKernelWidthInSeconds, the slower the filtered value will converge towards current input sample (and vice versa).
+    static float lowPassKernelWidthInSeconds = 1.0f;
+    // This next parameter is initialized to 2.0 per Apple's recommendation, or at least according to Brady! ;)
+    static float shakeDetectionThreshold = 2.0f;
 
-	Vector3 avrgAcc = Vector3.zero;
-	int countPos;
-	int countNeg;
-	int lastPeak;
-	int firstPeak;
-	public bool counting;
-	float timer;
+    static float lowPassFilterFactor = 0;
+    static Vector3 lowPassValue;
+    static Vector3 acceleration;
+    static Vector3 deltaAcceleration;
+    static bool is_first = true;
 
 	// Use this for initialization
 	void Start ()
@@ -30,50 +28,30 @@ public class ShakeDetector : MonoBehaviour
 	}
 
 	public bool isShaked(){
-		// read acceleration:
-		Vector3 curAcc = Input.acceleration; 
-		// update average value:
-		avrgAcc = Vector3.Lerp(avrgAcc, curAcc, avrgTime * Time.deltaTime);
-		// calculate peak size:
-		curAcc -= avrgAcc;
-		// variable peak is zero when no peak detected...
-		int peak = 0;
-		// or +/- 1 according to the peak polarity:
-		if (curAcc.y > peakLevel) peak = 1;
-		if (curAcc.y < -peakLevel) peak = -1;
-		// do nothing if peak is the same of previous frame:
-		if (peak == lastPeak) 
-			return false;
-		// peak changed state: process it
-		lastPeak = peak; // update lastPeak
-		if (peak != 0){ // if a peak was detected...
-			timer = 0; // clear end count timer...
-			if (peak > 0) // and increment corresponding count
-				countPos++;
-			else
-				countNeg++;
-			if (!counting){ // if it's the first peak...
-				counting = true; // start shake counting
-				firstPeak = peak; // save the first peak direction
-			}
-		} 
-		else // but if no peak detected...
-			if (counting){ // and it was counting...
-				timer += Time.deltaTime; // increment timer
-				if (timer > endCountTime){ // if endCountTime reached...
-					counting = false; // finish counting...
-					shakeDir = firstPeak; // inform direction of first shake...
-					if (countPos > countNeg) // and return the higher count
-						shakeCount = countPos;
-					else
-						shakeCount = countNeg;
-					// zero counters and become ready for next shake count
-					countPos = 0;
-					countNeg = 0;
-					return true; // count finished
-				}
-			}
-		return false;
+        if (is_first)
+        {
+            lowPassFilterFactor = accelerometerUpdateInterval / lowPassKernelWidthInSeconds;
+
+            lowPassValue = Vector3.zero;
+
+            shakeDetectionThreshold *= shakeDetectionThreshold;
+            lowPassValue = Input.acceleration;
+            is_first = false;
+            Debug.Log("is first if statement");
+        }
+
+        acceleration = Input.acceleration;
+        lowPassValue = Vector3.Lerp(lowPassValue, acceleration, lowPassFilterFactor);
+        deltaAcceleration = acceleration - lowPassValue;
+        Debug.Log("deltaAcceleration.sqrMagnitude  " + deltaAcceleration.sqrMagnitude);
+        if (deltaAcceleration.sqrMagnitude >= shakeDetectionThreshold)
+        {
+            // Perform your "shaking actions" here, with suitable guards in the if check above, if necessary to not, to not fire again if they're already being performed.
+            Debug.Log("Shake event detected at time " + Time.time);
+            return true;
+        }
+
+        return false;
 	}
 }
 
--- PokeDama/Assets/Scripts/MapDisplayManager.cs
+++ PokeDama/Assets/Scripts/MapDisplayManager.cs
@@ -1,15 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MapDisplayManager : MonoBehaviour {
+public class MapDisplayManager : MonoBehaviour, GameManager {
+    private static AndroidJavaClass bridge = null;
+    NetworkManager network;
+    string imei;
+    int number = 0;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+        network = FindObjectOfType<NetworkManager>();
+
+        Debug.Log("Unity before blue shareText callShareApp");
+        //Get Android context
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+        AndroidJavaObject app = activity.Call<AndroidJavaObject>("getApplicationContext");
+
+        imei = SystemInfo.deviceUniqueIdentifier;
+
+        //Install 
+        bridge = new AndroidJavaClass("com.madcamp.pokedamalib.BridgeActivity");
+        activity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+        {
+            bridge.CallStatic("callMap", app, imei);
+        }));
+
+        Debug.Log("Unity What is the name of gameobjects? " + gameObject.name);
+        Debug.Log("Unity after blue callShareApp");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (number > 100)
+        {
+            //server의 db에 query를 날리고 scene 정보를 받아오고 넘기던지 말던지 결정
+            network.RequestScene(imei);
+
+            number = 0;
+        }
+        number++;
+    }
+
+    IEnumerator RequestWithDelay(string data)
+    {
+
+        Debug.Log("Unity go to battle scene" + data);
+
+        return null;
+    }
+
+    public void handleResponse(string data)
+    {
+        JSONObject json = new JSONObject(data);
+
+        string sc = json.GetField("scene").str;
+
+        Debug.Log("Scene : " + json.GetField("scene").str);
+        Debug.Log("Scene : " + json.GetField("scene").str.Equals("BATTLE"));
+
+        if (json.GetField("scene").str.Equals("BATTLE"))
+        {
+            Application.LoadLevel(4);
+        }
+        else if (json.GetField("scene").str.Equals("DAMA"))
+        {
+            Application.LoadLevel(2);
+        }
+    }
 }
